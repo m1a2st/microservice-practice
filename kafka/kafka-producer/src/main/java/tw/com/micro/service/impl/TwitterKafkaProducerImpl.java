c5a3c4d2832903cc3eefed5270b059be219ea2aa
m1a2st <s7133700@gmail.com>
2023-12-16 16:34:17 +0800

    feat(kafka-producer): addProducerService
    add Producer implement

---
+++ kafka/kafka-producer/src/main/java/tw/com/micro/service/impl/TwitterKafkaProducerImpl.java
@@ -0,0 +1,64 @@
+package tw.com.micro.service.impl;
+
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import tw.com.micro.avro.model.TwitterAvroModel;
+import tw.com.micro.service.KafkaProducer;
+
+import javax.annotation.PreDestroy;
+
+
+@Service
+public class TwitterKafkaProducerImpl implements KafkaProducer<Long, TwitterAvroModel> {
+
+    private static final Logger log = LoggerFactory.getLogger(TwitterKafkaProducerImpl.class);
+
+    private final KafkaTemplate<Long, TwitterAvroModel> kafkaTemplate;
+
+    public TwitterKafkaProducerImpl(KafkaTemplate<Long, TwitterAvroModel> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public void send(String topicName, Long key, TwitterAvroModel message) {
+        log.info("Sending message='{}' to topic='{}'", message, topicName);
+        ListenableFuture<SendResult<Long, TwitterAvroModel>> kafkaResultFuture =
+                kafkaTemplate.send(topicName, key, message);
+        addCallback(topicName, message, kafkaResultFuture);
+    }
+
+    @PreDestroy
+    public void close() {
+        if (kafkaTemplate != null) {
+            log.info("Closing kafka producer!");
+            kafkaTemplate.destroy();
+        }
+    }
+
+    private void addCallback(String topicName, TwitterAvroModel message,
+                             ListenableFuture<SendResult<Long, TwitterAvroModel>> kafkaResultFuture) {
+        kafkaResultFuture.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                log.error("Error while sending message {} to topic {}", message.toString(), topicName, throwable);
+            }
+
+            @Override
+            public void onSuccess(SendResult<Long, TwitterAvroModel> result) {
+                RecordMetadata metadata = result.getRecordMetadata();
+                log.debug("Received new metadata. Topic: {}; Partition {}; Offset {}; Timestamp {}, at time {}",
+                        metadata.topic(),
+                        metadata.partition(),
+                        metadata.offset(),
+                        metadata.timestamp(),
+                        System.nanoTime());
+            }
+        });
+    }
+}
