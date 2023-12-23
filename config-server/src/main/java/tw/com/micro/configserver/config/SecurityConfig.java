6078e9e88952769595cf1f03bdac1d9ca3c32996
m1a2st <s7133700@gmail.com>
2023-12-23 15:22:27 +0800

    docs(docker): addConfigServer
    add config server to container

--- config-server/src/main/java/tw/com/micro/configserver/config/SecurityConfig.java
+++ config-server/src/main/java/tw/com/micro/configserver/config/SecurityConfig.java
@@ -10,7 +10,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/encrypt/**")
+        web.ignoring()
+                .antMatchers("/actuator/**")
+                .antMatchers("/encrypt/**")
                 .antMatchers("/decrypt/**");
         super.configure(web);
     }
